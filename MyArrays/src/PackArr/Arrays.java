697173d54321f4defe04b0c35d16f94daf82dc7e
skhan <skhan@DESKTOP-G3FDGEF>
2020-05-15 15:00:19 -0500

    Arrays and IF Else together

---
+++ MyArrays/src/PackArr/Arrays.java
@@ -0,0 +1,31 @@
+package PackArr;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// Here We are going to learn java Arrays
+		int []values = new int [100];
+		values[0]= 45;
+		values[67]= 77;
+		values[99]= 1000;
+		boolean hungry= true;
+		System.out.println(values[67]);
+		String [] words = new String[10];
+		words[0] = "My";
+		words[1] = "Name";
+		words[2] = "Salim";
+		System.out.println(words[2]);
+		
+		if (hungry) {
+		System.out.println("I am starving");
+		}else {
+		System.out.println(" I am full");
+		}
+		}
+		
+		
+		
+	}
+
+}
