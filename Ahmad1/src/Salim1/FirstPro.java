e31c52de4266c88eca393571b899ea3c061ec7d6
skhan <skhan@DESKTOP-G3FDGEF>
2020-05-15 13:08:56 -0500

    Second commit

---
+++ Ahmad1/src/Salim1/FirstPro.java
@@ -0,0 +1,16 @@
+package Salim1;
+
+public class FirstPro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+long BigNumber = 10000000L;
+System.out.println(BigNumber);
+double decimal = 34.67;
+System.out.println(BigNumber);
+System.out.println(decimal);
+boolean decision = false;
+char letter = 'J';
+	}
+
+}
